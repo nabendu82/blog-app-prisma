611408a6ca8fbf1ec67ef8956e84bedc663be6b2
nabendu82 <nabendu.biswas@gmail.com>
2022-07-07 05:25:31 +0530

    Part-1 done

---
+++ server/src/resolvers/Query.ts
@@ -0,0 +1,9 @@
+import { Context } from "../index";
+
+export const Query = {
+    posts: (_: any, __: any, { prisma }: Context) => {
+        return prisma.post.findMany({
+            orderBy: [{ createdAt: "desc" }]
+        });
+    }
+}
---
+++ server/src/schema.ts
@@ -0,0 +1,53 @@
+import { gql } from "apollo-server";
+
+export const typeDefs = gql`
+    type Query{
+        posts: [Post!]!
+    }
+
+    type Mutation{
+        postCreate(post: PostInput!): PostPayload!
+        postUpdate(postId: ID!, post: PostInput!): PostPayload!
+        postDelete(postId: ID!): PostPayload!
+    }
+
+    type UserError {
+        message: String!
+    }
+
+    type PostPayload {
+        userErrors: [UserError!]!
+        post: Post
+    }
+
+    input PostInput {
+        title: String
+        content: String
+    }
+
+
+    type Post {
+        id: ID!
+        title: String!
+        content: String!
+        createdAt: String!
+        published: Boolean!
+        user: User!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        profile: Profile!
+        posts: [Post!]!
+    }
+
+    type Profile {
+        id: ID!
+        bio: String!
+        user: User!
+    }
+
+
+`
